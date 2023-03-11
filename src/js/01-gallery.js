782a29472a02fd970973b06fd516067d37cfe0d3
PilyaP <ppaexpert@gmail.com>
2023-03-11 18:07:28 +0200

    54

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const paletteContainer = document.querySelector('.gallery');
+const imageMarkup = createGalleryMarkup(galleryItems); 
+
+paletteContainer.insertAdjacentHTML('beforeend', imageMarkup);
+
+function createGalleryMarkup(images) {
+  const markup = images.map(({ preview, original, description }) => { 
+    return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" 
+          src="${preview}" 
+          alt="${description}">
+        </a>
+      </div>
+    `;
+  })
+  .join('');
+  return markup;
+}
+const lightbox = new SimpleLightbox('.gallery a', {   captionsData: "alt",
+  captionDelay: 250, });
