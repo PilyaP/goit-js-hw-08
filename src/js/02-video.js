00662eb4e25d7e59b0e1777517a37545f1c7f116
PilyaP <ppaexpert@gmail.com>
2023-03-13 14:58:40 +0200

    22

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000));
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime).then(function(seconds) {
+    console.log('seek to', seconds, 'seconds');
+  }).catch(function(error) {
+    switch (error.name) {
+      case 'RangeError':
+        
+        break;
+
+      default:
+    
+        break;
+    }
+  });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+const feedbackKey = 'feedback-form-state';
+
+
+form.addEventListener('input', throttle(() => {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(feedbackKey, JSON.stringify(formData));
+}, 500));
+
+
+const savedData = localStorage.getItem(feedbackKey);
+if (savedData) {
+  const { email, message } = JSON.parse(savedData);
+  emailInput.value = email;
+  messageInput.value = message;
+}
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(formData);
+  localStorage.removeItem(feedbackKey);
+  emailInput.value = '';
+  messageInput.value = '';
+});
